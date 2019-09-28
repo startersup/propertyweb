59c7bab7f545a4766d74b6c8a698a2a389b29c17
vinodhan16 <vinodhan.thunderbolt@gmail.com>
2019-09-28 20:10:57 +0530

    root page updated

--- server/boot/root.js
+++ server/boot/root.js
@@ -8,6 +8,8 @@
 module.exports = function(server) {
   // Install a `/` route that returns server status
   var router = server.loopback.Router();
-  router.get('/', server.loopback.status());
+  router.get('/', function(req, res) {
+    res.render('index');
+  });
   server.use(router);
 };
